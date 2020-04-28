626e5e9bc0ef8f4bf806fc04568828e623cacd11
Richard Gao <coolrichardg54@gmail.com>
2020-04-28 15:30:29 -0700

    Updates

---
+++ precache-manifest.bb0131580227a34a71d241ec354529e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4364a51a2a38bdf4c7609a6e09894f8d",
+    "url": "/spiecy-web/index.html"
+  },
+  {
+    "revision": "1f748c21ddb097c0db23",
+    "url": "/spiecy-web/static/css/main.d8f0a624.chunk.css"
+  },
+  {
+    "revision": "eaffdb828a87274d43d1",
+    "url": "/spiecy-web/static/js/2.18349c7f.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/spiecy-web/static/js/2.18349c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f748c21ddb097c0db23",
+    "url": "/spiecy-web/static/js/main.e88d4683.chunk.js"
+  },
+  {
+    "revision": "0f86f730d01803baa5d2",
+    "url": "/spiecy-web/static/js/runtime-main.2f9bc980.js"
+  },
+  {
+    "revision": "a9190e14302f09d701b8a2ed56dcf006",
+    "url": "/spiecy-web/static/media/AquaAtsumeIcon.a9190e14.png"
+  },
+  {
+    "revision": "cd195c77c8ab02f4a8c6981c815e6f58",
+    "url": "/spiecy-web/static/media/AquaAtsumeInfo.cd195c77.png"
+  },
+  {
+    "revision": "83fd252cdbcc865c9eb585d2f38bcca0",
+    "url": "/spiecy-web/static/media/Bibendum.83fd252c.jpeg"
+  },
+  {
+    "revision": "13930685fbcc10950e4e082538097c0a",
+    "url": "/spiecy-web/static/media/JeffreyYang.13930685.png"
+  },
+  {
+    "revision": "4e6d3abb98d0cf20e65a67039deed7c9",
+    "url": "/spiecy-web/static/media/email.4e6d3abb.svg"
+  },
+  {
+    "revision": "7b6089969e3f07e35a906b79ec951910",
+    "url": "/spiecy-web/static/media/github.7b608996.svg"
+  }
+]);
